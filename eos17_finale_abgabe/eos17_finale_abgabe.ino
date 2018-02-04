a886ede7f465935592f0d4e45bf1c12512ef2227
Sebastian Kliem <sebastian-kliem@live.de>
2018-02-04 12:56:55 +0100

    refactor code

--- eos17_finale_abgabe/eos17_finale_abgabe.ino
+++ eos17_finale_abgabe/eos17_finale_abgabe.ino
@@ -2,35 +2,31 @@
 #include "Adafruit_VL53L0X.h"
 #include<Wire.h>
 
+// VL53L0X ToF sensor
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
-const int MPU_addr = 0x68; // I2C address of the MPU-6050
+VL53L0X_RangingMeasurementData_t measure;
+
+// gyroscope
+const int MPU_addr = 0x68;
 int16_t acX, acY, acZ, tmp, gyX, gyY, gyZ;
 
 // status LEDs
 const int LED1_PIN = 4;
 const int LED2_PIN = 5;
 
-// motor pin
+// motor pin (PWM)
 const int MOTOR_PIN = 9;
 
-const int baseSpeed     = 150;
-double currentSpeed  = baseSpeed;
-const int minSpeed      = 0;
-const int maxSpeed      = 255;
+// speed
+const int baseSpeed = 150;
+double currentSpeed = baseSpeed;
+const int minSpeed = 0;
+const int maxSpeed = 255;
 
+// distances
 double baseDistance = 160;
-
-int acceleration  = 10;
-int brake         = -30;
-
-
-double distance = 0;
-int lastDistance = 0;
-int deviation = 0;
-int deviationChange = 0;
-
-// ToF measure
-VL53L0X_RangingMeasurementData_t measure;
+double distance = 0; // current distance
+int lastDistance = 0; // last measured distance
 
 // PID
 double aggKp=4, aggKi=0.2, aggKd=1;
@@ -38,31 +34,15 @@ double consKp=1, consKi=0.1, consKd=0.25;
 PID myPID(&distance, &currentSpeed, &baseDistance, consKp, consKi, consKd, REVERSE);
 
 void setup() {
-
   Serial.begin(115200);
-  while (!Serial); // wait for Leonardo enumeration, others continue immediately
 
-  // ToF
-  Serial.println(F("Initializing I2C devices..."));
-  if (!lox.begin()) {
-    Serial.println(F("Failed to boot VL53L0X"));
-    while (1);
-  }
-
-  // Gyro
-  Wire.begin();
-  Wire.beginTransmission(MPU_addr);
-  Wire.write(0x6B);  // PWR_MGMT_1 register
-  Wire.write(0);     // set to zero (wakes up the MPU-6050)
-  Wire.endTransmission(true);
+  // I2C devices initialization
+  initializeDevices();
 
   // pins
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(MOTOR_PIN, OUTPUT);
-
-  // power
-  Serial.println(F("devices available\n"));
 
   // first ToF distance reading for moving average
   lox.rangingTest(&measure, false);
@@ -77,69 +57,34 @@ void setup() {
 
 
 void loop() {
-  Serial.print("Reading a measurement... ");
-  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
-
-  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+  // measure distance
+  lox.rangingTest(&measure, false);
+  if (measure.RangeStatus != 4) { // successful measurement
     int newDistance = measure.RangeMilliMeter;
     distance = ema(newDistance, distance);
     deviation = abs(distance - baseDistance);
-    
-    if (deviation < 25)
-    {  //we're close to setpoint, use conservative tuning parameters
+    lastDistance = distance;
+
+    // set speed using PID
+    if (deviation < 25) {
+      // close to setpoint, use conservative tuning parameters
       myPID.SetTunings(consKp, consKi, consKd);
     }
-    else
-    {
-       //we're far from setpoint, use aggressive tuning parameters
+    else {
+       // far from setpoint, use aggressive tuning parameters
        myPID.SetTunings(aggKp, aggKi, aggKd);
-    }
-    
+    }    
     myPID.Compute();
-
     
     Serial.print("Distance (mm): "); Serial.print(distance); 
-    Serial.print(" | currentSpeed: "); Serial.println(currentSpeed);
-//     Serial.print(" | deviation: "); Serial.print(deviation);
-//     Serial.print(" | deviationChange: "); Serial.println(deviationChange);
-
-/*
-    if ( deviation > 200 ) {
-      acceleration = 10;
-    } else if ( deviation > 100 ) {
-      acceleration = 5;
-    } else if ( deviation > 50 ) {
-      acceleration = 1;
-    } else if ( deviation = 0 ) {
-    } else if ( deviation > -45) {
-      if (deviationChange < -25) {
-        acceleration = -1;
-      }
-
-    } else if ( deviation > -90 ) {
-      if (deviationChange < -25) {
-        acceleration = -5;
-      }
-    }
-*/
-    lastDistance = distance;
-    //‚adjustSpeed(acceleration);
-    
-    /*
-      if(measure.RangeMilliMeter < (baseDistance) ) {
-      adjustSpeed(brake);
-      } else {
-        adjustSpeed(acceleration);
-      }
-    */
-  } else {
-    Serial.println(" out of range ");
+    Serial.print(" | currentSpeed: "); Serial.print(currentSpeed);
+    Serial.print(" | deviation: "); Serial.println(deviation);
+  } else { 
+    Serial.println("out of range");
   }
-  
 
+  // set motor speed
   analogWrite(MOTOR_PIN, currentSpeed);
-
-
 
   /*
     // Gyro
@@ -183,15 +128,27 @@ void loop() {
 }
 
 
-void adjustSpeed(int adjustment) {
-  currentSpeed += adjustment;
+void initializeDevices() {
+  Serial.println("Initializing I2C devices...");
+  
+  // ToF
+  if (!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while (1);
+  }
 
-  ( currentSpeed > 255 ) ? currentSpeed = maxSpeed : currentSpeed = currentSpeed;
-  ( currentSpeed < 0   ) ? currentSpeed = minSpeed : currentSpeed = currentSpeed;
+  // Gyro
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B); // PWR_MGMT_1 register
+  Wire.write(0); // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
 
+  Serial.println("I2C devices available");
 }
 
 // Exponential moving average
+// used for smooth distance mesures of ToF sensor
 int ema(int currentVal, int lastVal) {
   float weight = 0.1;
   return (weight * currentVal) + ((1 - weight) * lastVal);
